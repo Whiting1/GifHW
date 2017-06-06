661b049f57ef60af80dc9f7b5ebc3a39df85ba96
Whiting1 <11jhsballfpw@gmail.com>
2017-06-06 18:06:42 -0400

    finally worked

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+    // JS goes here 
+
+//neeed an array with theme
+
+// create buttons from that array
+
+//button results with Gif, paused
+
+// click the Gif begins animation 
+
+//grab a rating display that rating
+
+//allow users to input to create more Gifs
